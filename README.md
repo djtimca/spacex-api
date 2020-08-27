48b95d8ed117bed3d86504ef2c839c0a66e81b4b
Tim Empringham <tim.empringham@live.ca>
2020-08-27 15:26:04 -0400

    Added PyPi required files for release.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# spacex-api
+# Spacex-API (spacexpypi)
 PyPi Integration to the SpaceX API
+
+## Available Methods
+
+There are three available methods in this library which will return the json payload from the appropriate SpaceX API.
+
+### get_roadster_status()
+Returns the current status and details of the SpaceX Tesla Roadster.
+
+### get_next_launch()
+Returns the information on the next planned SpaceX launch.
+
+### get_upcoming_launches()
+Returns all upcoming launches that are planned with SpaceX.
---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spacexpypi", 
+    packages=["spacexpypi"],
+    version="0.0.1",
+    license='apache-2.0',
+    author="Tim Empringham",
+    author_email="tim.empringham@live.ca",
+    description="SpaceX API wrapper.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/djtimca/spacex-api",
+    download_url = 'https://github.com/djtimca/spacex-api/archive/v_001.tar.gz',
+    keywords = ['SpaceX', 'Launch', 'Roadster'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    python_requires='>=3.6',
+)
